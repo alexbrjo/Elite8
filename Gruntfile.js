180ebd9908c6a73f115430b4881a85b6b48b59cc
Alex Johnson <alexjo5000@gmail.com>
2017-03-20 21:03:00 -0400

    Fixing build
    * Readded karma-coverage

--- Gruntfile.js
+++ Gruntfile.js
@@ -71,6 +71,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('karma-coverage');
     grunt.loadNpmTasks('grunt-sloc');
     grunt.registerTask(
         'default', 
