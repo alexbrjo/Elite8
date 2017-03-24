0f498e7ad6e3b286c8d87f6cddbae6c8d8ae5b90
Alex Johnson <alexjo5000@gmail.com>
2017-03-24 18:18:43 -0400

    Updated karma config to fix build

--- karma.conf.js
+++ karma.conf.js
@@ -12,7 +12,10 @@ module.exports = function(config) {
         port: 9876,
         autoWatch: true,
         browsers: ['PhantomJS'],
-        captureTimeout: 30000, // 30 seconds
+        captureTimeout: 60000,
+        browserDisconnectTimeout: 30000,
+        browserDisconnectTolerance: 0,
+        browserNoActivityTimeout: 10000,
         singleRun: true,
         coverageReporter: {
             type : 'lcov', // change to lcovonly for no HTML
