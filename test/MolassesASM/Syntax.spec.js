056a7c01da8e0209fc18c9cfb6ae795e74da212a
Alex Johnson <alexjo5000@gmail.com>
2017-03-27 08:54:31 -0400

    Tried adding syntex spec back

---
+++ test/MolassesASM/Syntax.spec.js
@@ -0,0 +1,19 @@
+/**
+ * Tests the Assembly of Jump operations
+ */
+describe("AssemblySyntax", function() {
+
+    var sasm = new MolassesAssembler();
+    
+    /**
+     * Tests Assembly of JMP from Jump Group.
+     */
+    it("testMissingToken", function() {
+        // test address 0
+        var src = "JMP \n \n HLT";
+        expect(function() {
+            sasm.assemble(src, 8);
+        }).not.toThrow();
+        
+    });
+});
